2c3a2850f5459f8d71ac15757c518f78df26994c
Jeremy Loh <38811217+JeremyLoh@users.noreply.github.com>
2024-02-08 00:09:41 +0800

    add LinearSearchList

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,11 @@
+// To test: npx jest Linear
+// Type a partial name of the file
 export default function linear_search(haystack: number[], needle: number): boolean {
-
+    // return boolean representing if needle was found
+    for (let i = 0; i < haystack.length; i++) {
+        if (needle === haystack[i]) {
+            return true
+        }
+    }
+    return false
 }
