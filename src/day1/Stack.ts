7009193419304f5a8f23f33394c434b275f0e518
Jeremy Loh <38811217+JeremyLoh@users.noreply.github.com>
2024-02-10 14:06:22 +0800

    add stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
-export default class Stack<T> {
-    public length: number;
+// https://frontendmasters.com/courses/algorithms/stack/
+type SNode<T> = {
+    value: T
+    prev?: SNode<T>
+}
 
-    
+export default class Stack<T> {
+    public length: number
+    private head?: SNode<T>
 
     constructor() {
+        this.length = 0
+        this.head = undefined
     }
 
     push(item: T): void {
+        const v: SNode<T> = {value: item, prev: undefined}
+        this.length++
+        if (this.head == undefined) {
+            this.head = v
+            return
+        }
+        v.prev = this.head
+        this.head = v
+    }
 
-}
     pop(): T | undefined {
+        if (this.head == undefined) {
+            return undefined
+        }
+        this.length--
+        const value: T = this.head.value
+        const previous: SNode<T> | undefined = this.head.prev
+        this.head.prev = undefined
+        this.head = previous
+        return value
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
